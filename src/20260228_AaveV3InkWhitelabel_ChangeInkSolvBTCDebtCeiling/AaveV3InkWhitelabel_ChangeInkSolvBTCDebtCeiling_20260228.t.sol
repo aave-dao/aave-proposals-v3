fb42cb31b015485b7f87e2b68390f14586805165
Nandy Bâ <nandyba@yahoo.fr>
2026-02-28 02:01:04 +0100

    feat: add test to verify that pool act as expected

--- src/20260228_AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling/AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228.t.sol
+++ src/20260228_AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling/AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228.t.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3InkWhitelabel} from 'aave-address-book/AaveV3InkWhitelabel.sol';
+import {AaveV3InkWhitelabel, AaveV3InkWhitelabelAssets} from 'aave-address-book/AaveV3InkWhitelabel.sol';
 
 import 'forge-std/Test.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/src/ProtocolV3TestBase.sol';
 import {AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228} from './AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228.sol';
+import {GovV3Helpers} from 'aave-helpers/src/GovV3Helpers.sol';
 
 /**
  * @dev Test for AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228
@@ -13,10 +14,28 @@ import {AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228} from './AaveV3
  */
 contract AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228_Test is ProtocolV3TestBase {
   AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228 internal proposal;
+  address internal solBTCHolder = address(0x6CBAF2ae5a1034757DC6F77B42873e9580f211f6);
+  address solvBTC_underlying = address(0xaE4EFbc7736f963982aACb17EFA37fCBAb924cB3); // Ink SolvBTC update with import from address-book when available
 
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('ink'), 38737734);
     proposal = new AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228();
+  }
+
+  /**
+   * @notice Tests that borrowing with SolvBTC as collateral before debt ceiling change must fail.
+   */
+  function test_beforeChangeBorrowWithSolvBTCAsCollateralMustFail() public {
+    vm.startPrank(solBTCHolder);
+    vm.expectRevert();
+    AaveV3InkWhitelabel.POOL.borrow(
+      AaveV3InkWhitelabelAssets.GHO_UNDERLYING,
+      100 ether,
+      2,
+      0,
+      solBTCHolder
+    );
+    vm.stopPrank();
   }
 
   /**
@@ -30,5 +49,16 @@ contract AaveV3InkWhitelabel_ChangeInkSolvBTCDebtCeiling_20260228_Test is Protoc
       true,
       true
     );
+    skip(3600);
+    vm.startPrank(solBTCHolder);
+    AaveV3InkWhitelabel.POOL.setUserUseReserveAsCollateral(solvBTC_underlying, true);
+    AaveV3InkWhitelabel.POOL.borrow(
+      AaveV3InkWhitelabelAssets.GHO_UNDERLYING,
+      100 ether,
+      2,
+      0,
+      solBTCHolder
+    );
+    vm.stopPrank();
   }
 }
