c37d37f7cff2f334d896b5f9c2d53db623ac4fc3
Kogaroshi <25688223+Kogaroshi@users.noreply.github.com>
2026-09-24 12:41:07 +0100

    fix: named params

--- src/20260807_Multi_AaveV4RiskStewardsActivation/AaveV4Avalanche_AaveV4RiskStewardsActivation_20260807.sol
+++ src/20260807_Multi_AaveV4RiskStewardsActivation/AaveV4Avalanche_AaveV4RiskStewardsActivation_20260807.sol
@@ -30,10 +30,10 @@ contract AaveV4Avalanche_AaveV4RiskStewardsActivation_20260807 is IProposalGener
     AaveV3Avalanche.ACL_MANAGER.addRiskAdmin(AaveV4Avalanche.RISK_STEWARD);
 
     IRiskStewardV4(AaveV4Avalanche.RISK_STEWARD).setConfig(
-      RiskStewardV4Config.defaultConfig(
-        AaveV4Avalanche.HUB_CONFIGURATOR,
-        AaveV4Avalanche.SPOKE_CONFIGURATOR
-      )
+      RiskStewardV4Config.defaultConfig({
+        hubConfigurator: AaveV4Avalanche.HUB_CONFIGURATOR,
+        spokeConfigurator: AaveV4Avalanche.SPOKE_CONFIGURATOR
+      })
     );
   }
 }
--- src/20260807_Multi_AaveV4RiskStewardsActivation/AaveV4Ethereum_AaveV4RiskStewardsActivation_20260807.sol
+++ src/20260807_Multi_AaveV4RiskStewardsActivation/AaveV4Ethereum_AaveV4RiskStewardsActivation_20260807.sol
@@ -30,10 +30,10 @@ contract AaveV4Ethereum_AaveV4RiskStewardsActivation_20260807 is IProposalGeneri
     AaveV3Ethereum.ACL_MANAGER.addRiskAdmin(AaveV4Ethereum.RISK_STEWARD);
 
     IRiskStewardV4(AaveV4Ethereum.RISK_STEWARD).setConfig(
-      RiskStewardV4Config.defaultConfig(
-        AaveV4Ethereum.HUB_CONFIGURATOR,
-        AaveV4Ethereum.SPOKE_CONFIGURATOR
-      )
+      RiskStewardV4Config.defaultConfig({
+        hubConfigurator: AaveV4Ethereum.HUB_CONFIGURATOR,
+        spokeConfigurator: AaveV4Ethereum.SPOKE_CONFIGURATOR
+      })
     );
   }
 }
